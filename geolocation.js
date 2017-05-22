4a268c7ed8a3ba7f8c48970a314248700f7c300b
Florian Schaeffler <flo@hikey.io>
2017-05-22 18:13:18 +0300

    updated package dependencies

--- geolocation.js
+++ geolocation.js
@@ -12,38 +12,38 @@ var error = new ReactiveVar(null);
 
 // options for watchPosition
 var options = {
-  enableHighAccuracy: true,
-  maximumAge: 0,
-  timeout: 10000
+	enableHighAccuracy: true,
+	maximumAge: 0,
+	timeout: 10000
 };
 
 var onError = function (newError) {
-  error.set(newError);
+	error.set(newError);
 };
 
 var onPosition = function (newLocation) {
-  location.set(newLocation);
-  error.set(null);
+	location.set(newLocation);
+	error.set(null);
 };
 
 var startWatchingPosition = function () {
-  if (! watchingPosition && navigator.geolocation) {
-    watchId = navigator.geolocation.watchPosition(onPosition, onError, options);
-    watchingPosition = true;
-  }
+	if (! watchingPosition && navigator.geolocation) {
+		watchId = navigator.geolocation.watchPosition(onPosition, onError, options);
+		watchingPosition = true;
+	}
 };
 
 var stopWatchingPosition = function () {
-  if (watchId && navigator.geolocation) {
-    navigator.geolocation.clearWatch(watchId);
-  }
-  watchingPosition = false;
+	if (watchId && navigator.geolocation) {
+		navigator.geolocation.clearWatch(watchId);
+	}
+	watchingPosition = false;
 }
 
 var getCurrentPosition = function () {
-  if (! watchingPosition && navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(onPosition, onError, options);
-  }
+	if (! watchingPosition && navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(onPosition, onError, options);
+	}
 }
 
 // exports
@@ -53,71 +53,71 @@ var getCurrentPosition = function () {
  * @namespace
  */
 Geolocation = {
-  setOptions: function (opts) {
-    if (opts) {
-      options.enableHighAccuracy =  opts.enableHighAccuracy || true,
-      options.maximumAge =  opts.maximumAge || 0,
-      options.timeout =  opts.timeout || 10000
-    }
-    return options;
-  },
-  /**
-   * @summary Get the current geolocation error
-   * @return {PositionError} The
-   * [position error](https://developer.mozilla.org/en-US/docs/Web/API/PositionError)
-   * that is currently preventing position updates.
-   */
-  error: function () {
-    startWatchingPosition();
-    return error.get();
-  },
+	setOptions: function (opts) {
+		if (opts) {
+			options.enableHighAccuracy =  opts.enableHighAccuracy || true,
+			options.maximumAge =  opts.maximumAge || 0,
+			options.timeout =  opts.timeout || 10000
+		}
+		return options;
+	},
+	/**
+	 * @summary Get the current geolocation error
+	 * @return {PositionError} The
+	 * [position error](https://developer.mozilla.org/en-US/docs/Web/API/PositionError)
+	 * that is currently preventing position updates.
+	 */
+	error: function () {
+		startWatchingPosition();
+		return error.get();
+	},
 
-  /**
-   * @summary Get the current location
-   * @return {Position | null} The
-   * [position](https://developer.mozilla.org/en-US/docs/Web/API/Position)
-   * that is reported by the device, or null if no position is available.
-   */
-  currentLocation: function (isWatching) {
-    if (isWatching) {
-      startWatchingPosition();
-    } else {
-      stopWatchingPosition();
-      getCurrentPosition();
-    }
-    return location.get();
-  },
-  // simple version of location; just lat and lng
+	/**
+	 * @summary Get the current location
+	 * @return {Position | null} The
+	 * [position](https://developer.mozilla.org/en-US/docs/Web/API/Position)
+	 * that is reported by the device, or null if no position is available.
+	 */
+	currentLocation: function (isWatching) {
+		if (isWatching) {
+			startWatchingPosition();
+		} else {
+			stopWatchingPosition();
+			getCurrentPosition();
+		}
+		return location.get();
+	},
+	// simple version of location; just lat and lng
 
-  /**
-   * @summary Get the current latitude and longitude
-   * @return {Object | null} An object with `lat` and `lng` properties,
-   * or null if no position is available.
-   */
-  latLng: function (isWatching) {
-    var loc = Geolocation.currentLocation(isWatching);
+	/**
+	 * @summary Get the current latitude and longitude
+	 * @return {Object | null} An object with `lat` and `lng` properties,
+	 * or null if no position is available.
+	 */
+	latLng: function (isWatching) {
+		var loc = Geolocation.currentLocation(isWatching);
 
-    if (loc) {
-      return {
-        lat: loc.coords.latitude,
-        lng: loc.coords.longitude
-      };
-    }
+		if (loc) {
+			return {
+				lat: loc.coords.latitude,
+				lng: loc.coords.longitude
+			};
+		}
 
-    return null;
-  },
+		return null;
+	},
 
-  loc: function () {
-    return location.get();
-  },
+	loc: function () {
+		return location.get();
+	},
 
-  stopWatching: function() {
-    stopWatchingPosition();
-    return null;
-  },
+	stopWatching: function() {
+		stopWatchingPosition();
+		return null;
+	},
 
-  startWatching: function() {
-    startWatchingPosition();
-    return null;
-  }
+	startWatching: function() {
+		startWatchingPosition();
+		return null;
+	}
 };
--- package.js
+++ package.js
@@ -1,23 +1,21 @@
 Package.describe({
-  name: 'phund:geolocation',
-  version: '1.0.1_1',
-  // Brief, one-line summary of the package.
-  summary: 'Provides geolocation on desktop and mobile. Have optional for reactive',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/phund/geolocation.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	name: 'phund:geolocation',
+	version: '1.0.2',
+	summary: 'Provides geolocation on desktop and mobile. Have optional for reactive',
+	git: 'https://github.com/phund/geolocation.git',
+	documentation: 'README.md'
 });
 
 Cordova.depends({
-  "cordova-plugin-geolocation": "2.4.0"
+	"cordova-plugin-geolocation": "2.4.3"
 });
 
-Package.onUse(function(api) {
-  api.use(["reactive-var"]);
-  api.versionsFrom("METEOR@1.2");
-  api.use("isobuild:cordova@5.2.0");
-  api.add_files(["geolocation.js"], "client");
-  api.export("Geolocation", "client");
+Package.onUse(function (api) {
+	api.versionsFrom('1.2.1');
+	api.use('reactive-var');
+	api.use('tracker');
+	api.use('underscore');
+	api.use("isobuild:cordova@5.2.0");
+	api.addFiles('geolocation.js', 'client');
+	api.export("Geolocation", "client");
 });
